5860c5ab0de9e832126540313512ce02c2af4af3
Rutu1591 <kulalrutuja@gmail.com>
2020-06-16 16:29:40 +0530

    New Commit

---
+++ Maven_Jenkins/src/main/java/com/maven/jenkins/Maven_Jenkins/App.java
@@ -0,0 +1,13 @@
+package com.maven.jenkins.Maven_Jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Rutu!" );
+    }
+}
